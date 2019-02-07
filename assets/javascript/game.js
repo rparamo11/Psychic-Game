ffc1bb40d7400d1ab2658d8c798df27a5e3070f3
rparamo <rparamo11@gmail.com>
2019-02-06 23:22:48 -0600

    fixed .push() issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,13 @@ var userGuesses = [];
 
 function resetGlobalVariables () {
     guessLeft = 10;
-    userGuessess = [];
+    userGuesses = [];
 }
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    userGuesses.concat(userGuess);
+    userGuesses.push(userGuess);
 
     if (userGuess === computerGuess) {
         wins++;
@@ -28,13 +28,13 @@ document.onkeyup = function (event) {
         }
     }
 
-    
     var html =
     
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +
     "<p>Guesses left: " + guessLeft + "</p>" +
-    "<p>You chose: " + userGuess + "</p>" ;
+    //"<p>Letters Guessed: " + userGuesses.join(' ') + "</p>"
+    "<p>You chose: " + userGuesses.join(' ') + "</p>" ;
 
 
   // Set the inner HTML contents of the #game div to our html string
